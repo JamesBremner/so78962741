6b4bc4a48d2716123c41bb02dc7ca6e7cc118b1b
JamesBremner <james@ravenspoint.com>
2024-09-12 20:52:09 -0400

    code

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,87 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <wex.h>
-#include "cStarterGUI.h"
 
-class cGUI : public cStarterGUI
+int N;
+std::vector<std::string> vcodes;
+std::vector<std::pair<int, int>> vlinks;
+
+void generate(int n)
 {
-public:
-    cGUI()
-        : cStarterGUI(
-              "Starter",
-              {50, 50, 1000, 500}),
-          lb(wex::maker::make < wex::label >(fm))
+    vcodes.clear();
+    N = n;
+    for (int i = 0; i < n; i++)
+        vcodes.push_back("A");
+    for (int i = 0; i < n; i++)
+        vcodes.push_back("B");
+
+    vlinks.push_back(std::make_pair(1, 4));
+    vlinks.push_back(std::make_pair(2, 4));
+}
+/*
+I assume that '4' indicates the first object in the second group
+
+Convert the link indices to zero-based indepedently in each group
+
+*/
+void zerobase()
+{
+    for (auto &l : vlinks)
     {
-        lb.move(50, 50, 100, 30);
-        lb.text("Hello World");
-
-        show();
-        run();
+        l.first -= 1;
+        l.second -= N + 1;
     }
+}
 
-private:
-    wex::label &lb;
-};
+void link()
+{
+    for (auto &link : vlinks)
+    {
+        for (int object = 0; object < N; object++)
+            if (object == link.first)
+                vcodes[object] += "A";
+            else
+                vcodes[object] += "B";
+        for (int object = 0; object < N; object++)
+            if (object == link.second)
+                vcodes[N + object] += "A";
+            else
+                vcodes[N + object] += "C";
+    }
+}
+
+void makeUnique()
+{
+    std::vector<std::string> vus;
+    char vc[] = {'A', 'B', 'C'};
+    do
+    {
+        // std::cout << vc[0] << ' ' << vc[1] << ' ' << vc[2] << '\n';
+        std::string us = "xxx";
+        us[0] = vc[0];
+        us[1] = vc[1];
+        us[2] = vc[2];
+        vus.push_back(us);
+    } while (std::next_permutation(vc, vc + 3));
+    for (int i = 0; i < N; i++)
+    {
+        vcodes[i] += vus[i];
+        vcodes[N + i] += vus[i];
+    }
+}
+
+void displayCodes()
+{
+    for (auto &c : vcodes)
+        std::cout << c << "\n";
+}
 
 main()
 {
-    cGUI theGUI;
+    generate(3);
+    zerobase();
+    link();
+    makeUnique();
+    displayCodes();
     return 0;
 }
